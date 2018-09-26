f26da74fbbc04878a04ca440fd96632798a69a5b
Ophiel solutions <ophielsolutions@gmail.com>
2018-09-26 23:17:17 +0530

    index edit

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
  ?>
  <div id="wrapper">
        <div class="overlay"></div>
-
        <!-- Sidebar -->
        <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
            <ul class="nav sidebar-nav">
@@ -47,122 +46,93 @@
                </li>
            </ul>
        </nav>
-       <!-- /#sidebar-wrapper -->
-
-       <!-- Page Content -->
 
            <div class="">
              <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
-                 <span class="hamb-top"></span>
-           <span class="hamb-middle"></span>
-         <span class="hamb-bottom"></span>
+                <span class="hamb-top"></span>
+           			<span class="hamb-middle"></span>
+         				<span class="hamb-bottom"></span>
              </button>
                <div class="row">
                    <div class="" id="main_banner">
                      <div id="carousel-slider" class="carousel slide" data-ride="carousel">
-     <!-- Indicators
-     <ol class="carousel-indicators visible-xs">
-       <li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
-       <li data-target="#carousel-slider" data-slide-to="1"></li>
-       <li data-target="#carousel-slider" data-slide-to="2"></li>
-     </ol>
-     -->
 
-     <div class="carousel-inner">
-       <div class="item active">
-         <img src="images/our_img_1.jpg" class="home_banner_img" alt="" >
-         <div class="carousel-caption">
-         <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
-           <div class="col-md-4">
-           </div>
+								     <div class="carousel-inner">
+								       <div class="item active">
+									         <img src="images/our_img_1.jpg" class="home_banner_img" alt="" >
+									         <div class="carousel-caption">
+									         		<div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
+									           <div class="col-md-4">
+									           </div>
+									         </div>
+									          </div>
+								         </div>
 
-         </div>
-             </div>
-         </div>
+								         <div class="item">
+									         <img src="images/our_img_2.jpg" class="home_banner_img" alt="">
+									         <div class="carousel-caption">
+									         <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
+									           <div class="col-md-4">
+									           </div>
+									         </div>
+									          </div>
+								         </div>
 
-         <div class="item">
-         <img src="images/our_img_2.jpg" class="home_banner_img" alt="">
-         <div class="carousel-caption">
-         <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
-           <div class="col-md-4">
-           </div>
+								         <div class="item">
+									         <img src="images/our_img_3.jpg" class="home_banner_img" alt="">
+									         <div class="carousel-caption">
+									         <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
+									           <div class="col-md-4">
+									           </div>
 
-         </div>
-             </div>
+									         </div>
+									             </div>
+								         </div>
 
-         </div>
-         <div class="item">
-         <img src="images/our_img_3.jpg" class="home_banner_img" alt="">
-         <div class="carousel-caption">
-         <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
-           <div class="col-md-4">
-           </div>
+								         <div class="item">
+									         <img src="images/our_img_4.jpg" class="home_banner_img" alt="">
+											         <div class="carousel-caption">
+												         <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
+												           <div class="col-md-4">
+												           </div>
+												         </div>
+									             </div>
+								         </div>
 
-         </div>
-             </div>
-         </div>
-
-         <div class="item">
-         <img src="images/our_img_4.jpg" class="home_banner_img" alt="">
-         <div class="carousel-caption">
-         <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
-           <div class="col-md-4">
-           </div>
-
-         </div>
-             </div>
-         </div>
-
-         <div class="item">
-         <img src="images/our_img_5.jpg" class="home_banner_img" alt="">
-         <div class="carousel-caption">
-         <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
-           <div class="col-md-4">
-           </div>
-
-         </div>
-             </div>
-         </div>
-
-         <div class="item">
-         <img src="images/our_img_6.jpg" class="home_banner_img" alt="">
-         <div class="carousel-caption">
-         <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
-           <div class="col-md-4">
-           </div>
-
-         </div>
-             </div>
-         </div>
-
-     </div>
-
-     <a class="left carousel-control hidden-xs" href="#carousel-slider" data-slide="prev">
-
-     </a>
-
-     <a class=" right carousel-control hidden-xs"href="#carousel-slider" data-slide="next">
-
-     </a>
-   </div> <!--/#carousel-slider-->
-
-       </div>
+								         <div class="item">
+									         <img src="images/our_img_5.jpg" class="home_banner_img" alt="">
+									         <div class="carousel-caption">
+										         <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
+										           <div class="col-md-4">
+										           </div>
+										         </div>
+									          </div>
+								         </div>
 
 
-			 <div class="well" style="background:black; height:600px; margin:0px; align: center;">
-				 <div class="row">
-					 <h1 >BOOK YOUR EXPERIENCE</h1>
-				 </div>
+								     </div>
 
-			 </div>
+								     <a class="left carousel-control hidden-xs" href="#carousel-slider" data-slide="prev"></a>
 
-			 <div class="well" style="background:white; height:600px; margin:0px;">
+								     <a class=" right carousel-control hidden-xs"href="#carousel-slider" data-slide="next"></a>
+								   </div> <!--/#carousel-slider-->
 
-			 </div>
+       	 					</div>
 
 
-    </div>
-  </div>
+<!-- Clients ================================================== -->
+<section id="clients"   style="background-color:white;">
+	<div class="wrapper">
+		<div class="container">
+				<h1>heeee</h1>
+		</div>
+
+	</div>
+</section>
+
+
+    					</div>
+  				</div>
        <!-- /#page-content-wrapper -->
 
    </div>
