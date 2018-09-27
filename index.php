60a21172162669db9b6d89784ec15a801e3028e5
Ophiel solutions <ophielsolutions@gmail.com>
2018-09-27 11:31:08 +0530

    upload new files

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	include('includes/header.php');
+	include('header.php');
 
  ?>
  <div id="wrapper">
@@ -120,14 +120,59 @@
        	 					</div>
 
 
-<!-- Clients ================================================== -->
-<section id="clients"   style="background-color:white;">
-	<div class="wrapper">
-		<div class="container">
-				<h1>heeee</h1>
+<section id="hero" class="">
+	<div class="container card">
+		<div class="text-center">
+			<h1 class="wow bounceInUp" data-wow-offset="0" data-wow-delay="0.2s">BOOK YOUR EXPERIENCE</h1>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="col-sm-4">
+						<div class="dropdown">
+							 <button class="btn btn-default dropdown-toggle"
+							 type="button" id="menu1" data-toggle="dropdown">Applications
+							 <span class="caret"></span></button>
+							 <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+							 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">
+							 My Apllications</a></li>
+							 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">
+							 Staff Board</a></li>
+							 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Apply
+							 </a>
+							 </li>
+
+
+							 </ul>
+							 </div>
+					</div>
+					<div class="col-sm-4">
+						<p>hello</p>
+					</div>
+					<div class="col-sm-4">
+						<h1>hee</h1>
+					</div>
+			</div>
+		</div>
 		</div>
 
+
 	</div>
+</section>
+
+<section id="about" class="">
+	<div class="wrapsection">
+<div class="parallax-overlay"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="text-center">
+					<h1 class="wow bounceInUp" data-wow-offset="0" data-wow-delay="0.5s">Hello</h1>
+					<div class="wow bounceInUp" data-wow-offset="0" data-wow-delay="0.5s">
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
 </section>
 
 
@@ -139,6 +184,9 @@
    <!-- /#wrapper -->
 
 
+
+
+ <script>wow = new WOW({}).init();</script>
 
 
 
