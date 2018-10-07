48b9f3bc34e5729e2333a73082201d90a307d1b7
Ophiel solutions <ophielsolutions@gmail.com>
2018-10-07 13:55:59 +0530

    now ok

---
+++ pages/index.php
@@ -0,0 +1,154 @@
+<?php  //include('../includes/header.php');
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+
+    	<meta name="viewport" content="width=device-width, initial-scale=1.1, shrink-to-fit=no">
+    	<meta name="description" content="docter helping app" />
+    	<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+    	<meta name="author" content="Ophiel.lk" />
+
+
+    	<link rel="stylesheet" href="../assests/bootstrap/css/animate.css">
+    	<link rel="stylesheet" href="../assests/bootstrap/css/animate.min.css">
+
+    	<!-- bootstrap theme-->
+    	<link rel="stylesheet" href="../assests/bootstrap/css/bootstrap-theme.min.css">
+    	<link rel="stylesheet" href="../assests/bootstrap/css/bootstrap-theme.css">
+
+    	<!-- bootstrap -->
+    	<link rel="stylesheet" href="../assests/bootstrap/css/bootstrap.min.css">
+    	<link rel="stylesheet" href="../assests/bootstrap/css/bootstrap.css">
+
+
+    	<!-- font awesome -->
+    	<link rel="stylesheet" href="../assests/font-awesome/css/font-awesome.css">
+    	<link rel="stylesheet" href="../assests/font-awesome/css/font-awesome.min.css">
+
+
+      <style media="screen">
+      #parallax {
+height:100%;
+width:100%;
+top:0;
+position:fixed;
+	background-image:url("../images/Our_img_1.jpg");
+/*	background-image:url(http://unsplash.it/1600/600?random=1);*/
+background-position: center;
+background-repeat:no-repeat;
+-webkit-background-size: cover;
+-moz-background-size: cover;
+-o-background-size: cover;
+background-size: cover;
+z-index: -100;
+}
+/*	Hero	==================================================*/
+#hero {
+	margin-top:140px;
+	height: 400px;
+	align-items: center;
+	text-align: center;
+	padding:0;
+	color:black;
+}
+      </style>
+
+
+      <!-- jquery -->
+    	<script src="../assests/jquery/jquery.min.js"></script>
+      <!-- jquery ui -->
+      <link rel="stylesheet" href="../assests/jquery-ui/jquery-ui.min.css">
+      <script src="../assests/jquery/jquery-ui.min.js"></script>
+
+      <!-- bootstrap js -->
+    	<script src="../assests/bootstrap/js/bootstrap.min.js"></script>
+    	  <script src="../assests/jquery/jquery-1.11.1.min.js"></script>
+
+    	<script src="../assests/bootstrap/js/bootstrap.js"></script>
+
+    	<script src="../assests/bootstrap/js/wow.min.js"></script>
+
+
+  </head>
+  <body>
+<div class="" id="parallax">
+
+</div>
+<section id="hero" class="">
+	<div class="container">
+			<h1 class="wow bounceInUp" data-wow-offset="0" data-wow-delay="0.2s">Hello</h1>
+			<div class="wow bounceInUp" data-wow-offset="0" data-wow-delay="0.5s">
+				<h2 class="wow bounceInUp"><span>Clean & Modern Design Template</span></h2>
+			</div>
+	</div>
+</section>
+
+<section id="service" class="well section">
+	<div class="wrapsection">
+		<div class="parallax-overlay" style="background-color: #01b0d1;opacity:0.7;">
+		</div>
+		<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<!-- Title -->
+							<div class="maintitle wow bounceInDown" data-wow-offset="0" data-wow-delay="0.2s">
+								<h1 id="sec_d"><strong>OUR <span class="lighter">SERVICES</span></strong></h1>
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class="row">
+						<div class="col-md-12 service_panel ">
+							<div class="text-justify">
+								<div class="wow bounceInDown"  data-wow-delay="0.3s">
+									<h3>Hotel Reservations</h3>
+								</div>
+								<br>
+								<div class="wow fadeInRight" data-wow-offset="0" data-wow-delay="0.3s">
+					          <p>Splendour travels doing hotel reservations as one of our major duty. Most clients do their bookings through us because of we have a huge relationship with best hotels around the island. Also as our experienced journey in tourism & Hospitality we have most cheap able rates for our clients. When you see accommodations on internet, it may be misled you with beautiful pictures. But ultimately the result is dissatisfaction of our clients.
+												<br>
+												So Splendour travels is guaranteed for our resrvations
+											</p>
+					      </div>
+							</div>
+						</div>
+						<div class="col-md-4 service_panel ">
+							<div class="text-justify">
+								<div class="wow bounceInDown"  data-wow-delay="0.6s">
+									<h3>Creating itinerates </h3>
+								</div>
+								<br>
+								<div class="wow fadeInRight" data-wow-offset="0" data-wow-delay="0.6s">
+					          <p>Itinery is the most important part of any tour. Splendour travels is not going to promote our own itineries again and again because of clients are vary from one to another. As their request we are preparing more personalized itineries as client’s requests with higher careness about you.</p>
+					      </div>
+							</div>
+						</div>
+						<div class="col-md-4 service_panel ">
+							<div class="text-justify">
+								<div class="wow bounceInDown"  data-wow-delay="0.9s">
+									<h3>SERVICE ONE</h3>
+								</div>
+								<br>
+								<div class="wow fadeInRight" data-wow-offset="0" data-wow-delay="0.9s">
+					          <p>Sanit Inventions (Pvt) Ltd is IT Solutions Company recognized for New and innovative, customized and cost effective solutions.
+					  				As a fully integrated solutions provider, we offer you a full range of professional services that e-businesses need, by providing strategy and implementation of IT solutions for businesses in the new economy. We help to share your business information and IT resources by managing your workstations, computer networks and Internet and email requirements and communication needs via PABX Systems, security requirement through camera system etc...
+					  				</p>
+					      </div>
+							</div>
+						</div>
+					</div>
+				</div>
+	</div>
+</section>
+
+
+
+
+     <script>wow = new WOW({}).init();</script>
+
+  </body>
+</html>
